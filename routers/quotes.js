d7c1b7fd6eeac0fc113f460f17c55b94b6234e7c
Laksito Pamilih <laksito@protonmail.com>
2023-09-05 11:21:10 +0700

    add route to create quote

--- routers/quotes.js
+++ routers/quotes.js
@@ -4,12 +4,6 @@ const { getRandomElement } = require('../utils');
 
 const router = express.Router()
 
-router.get('/random', (req, res) => {
-  res.send({
-    quote: getRandomElement(quotes)
-  })
-})
-
 router.get('/', (req, res) => {
   const { person } = req.query
 
@@ -20,6 +14,25 @@ router.get('/', (req, res) => {
   } else {
     res.send({ quotes })
   }
+})
+
+router.post('/', (req, res) => {
+  const { quote, person } = req.query
+
+  if (quote && person) {
+    const newQuote = { quote, person }
+  
+    quotes.push(newQuote)
+    res.status(201).send({ quote: newQuote })
+  } else {
+    res.status(400).send('invalid data')
+  }
+})
+
+router.get('/random', (req, res) => {
+  res.send({
+    quote: getRandomElement(quotes)
+  })
 })
 
 module.exports = router
